6c267fe318a29eb5c9be6d5a037e747a8283c233
Asim-Coding <muhammadasim19955@gmail.com>
2022-05-06 15:26:51 +0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CrisesListComponent } from './crises-list/crises-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot([
-    {path:'',redirectTo:'/hero-list',pathMatch:'full'},
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot([ 
     {path: 'crises-list', component: CrisesListComponent},
     {path: 'hero-list', component: HeroListComponent},
+    {path:'',redirectTo:'/hero-list',pathMatch:'full'},
+    {path:' ** ', component:PagenotfoundComponent}
   ]), ],
-  declarations: [ AppComponent, HelloComponent,CrisesListComponent,HeroListComponent ],
+  declarations: [ AppComponent, HelloComponent,CrisesListComponent,HeroListComponent,PagenotfoundComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
